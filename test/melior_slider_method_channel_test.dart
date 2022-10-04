5c465bc45b3a651796ceb227db62e99f62179d22
LinhNV <vietlinhtspt@gmail.com>
2022-10-04 10:09:00 +0700

    test public 0.0.1

--- test/melior_slider_method_channel_test.dart
+++ test/melior_slider_method_channel_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:melior_slider/melior_slider_method_channel.dart';
+import 'package:melior_slideshow/melior_slider_method_channel.dart';
 
 void main() {
   MethodChannelMeliorSlider platform = MethodChannelMeliorSlider();
